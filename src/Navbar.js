bb7413ab14a69003530cca199a9f6c50973711b5
Kolyamilk <Kolyamilk@mail.ru>
2024-07-02 10:29:52 +0300

    edit logic to pomidoroTimer

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 export const Navbar = ({ title }) => {
+    const [userName, setUserName] = useState('')
     return (
         <View style={classes.navbar}>
 
             <View style={classes.image}>
                 <TouchableOpacity TouchableOpacity={0.2} >
                     <Image source={require('../assets/user.png')} style={classes.image} />
+                    <Text style={classes.textUser}>{userName}</Text>
                 </TouchableOpacity>
-            </View>
 
+            </View>
             <View style={classes.text}>
                 <Text style={classes.text}>{title}</Text>
             </View>
@@ -29,6 +31,12 @@ const classes = StyleSheet.create({
         justifyContent: 'center',
         color: 'white',
         fontSize: 30,
+    },
+    textUser: {
+        color: 'white',
+        fontSize: 16,
+        width: 400
+
     },
     image: {
         width: 40,
--- src/PomidoroTimer.js
+++ src/PomidoroTimer.js
@@ -1,25 +1,69 @@
-import React from 'react'
-import { Text, View, StyleSheet, TouchableOpacity, Image, Button, ImageBackground } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Text, View, StyleSheet, TouchableOpacity,  ImageBackground} from 'react-native'
+
+
 
 export const PomidoroTimer = () => {
+    const [mainTimer, setMainTimer] = useState(25 * 60);
+    const [breakTimer, setBreakTimer] = useState(5 * 60);
+    const [isActive, setIsActive] = useState(false);
+    const [onBreak, setOnBreak] = useState(false);
 
+
+
+    useEffect(() => {
+        let interval = null;
+        if (isActive) {
+            interval = setInterval(() => {
+                if (!onBreak && mainTimer > 0) {
+                    setMainTimer(mainTimer => mainTimer - 1)
+                } else if (!onBreak && mainTimer === 0) {
+                    setOnBreak(true)
+                } else if (onBreak && breakTimer > 0) {
+                    setBreakTimer(breakTimer => breakTimer - 1)
+                } else if (onBreak && breakTimer === 0) {
+                    setIsActive(false);
+                    setOnBreak(false);
+                    setMainTimer(25 * 60);
+                    setBreakTimer(5 * 60);
+                }
+            }, 1000);
+        } else if (!isActive && (mainTimer !== 0 || breakTimer !== 0)) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [isActive, mainTimer, breakTimer, onBreak]);
+
+
+    const toggle = () => setIsActive(!isActive);
+
+
+
+    
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / 60)
+        const seconds = time % 60
+
+        return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+    }
 
     return (
         <View style={classes.container}>
-
-            <ImageBackground resizeMode="cover" style={classes.image} source={require('../assets/pomidorka.png')}>
-                <Text style={classes.text}>PLAY</Text>
-            </ImageBackground>
-
-
-
+        
+        <Text style={classes.text}>{onBreak ? formatTime(breakTimer) : formatTime(mainTimer)}</Text>
+            <TouchableOpacity onPress={toggle}>
+                <ImageBackground resizeMode="cover" style={classes.image} source={isActive ? require('../assets/pause.png') : require('../assets/play.png')}>
+                </ImageBackground>
+            </TouchableOpacity>
+  
         </View>
     )
 }
 
+
 const classes = StyleSheet.create({
     container: {
-        flexDirection: 'row',
+        flexDirection: '',
         justifyContent: 'center',
         alignItems: 'center',
         height: '100%'
@@ -28,12 +72,11 @@ const classes = StyleSheet.create({
     image: {
         justifyContent: 'center',
         alignItems: 'center',
-        width: 300,
-        height: 300
+        width: 100,
+        height: 100
     },
     text: {
-        fontSize: 40,
-        color:'white'
+        fontSize: 100,
+        color: 'white'
     }
-
-})
+});
